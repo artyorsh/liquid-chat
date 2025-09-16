225b2fae6d76116451d0d30c6fcff9d2e505d5b1
Artur Y <artur@artyorsh.me>
2025-09-16 08:33:31 +0200

    load container in index.js (before <App />)

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-import registerRootComponent from 'expo/src/launch/registerRootComponent';
-
 import 'core-js/proposals/reflect-metadata';
+import registerRootComponent from 'expo/src/launch/registerRootComponent';
+import { Container } from 'inversify';
+
+import '@/uilib';
+import { AuthModule } from '@/auth';
+import { HomeScreenModule } from '@/home';
+import { LogModule } from '@/log';
+import { ModalModule } from '@/modal';
+import { ProcessInfoModule } from '@/process-info';
+import { PushNotificationModule } from '@/push-notification';
+import { RouterModule } from '@/router';
+import { SplashScreenModule } from '@/splash';
+import { UserModule } from '@/user';
+
 import { App } from './src/app';
 
-registerRootComponent(App);
+export const container = new Container();
+
+container.load(
+  AuthModule,
+  LogModule,
+  RouterModule,
+  ProcessInfoModule,
+  PushNotificationModule,
+  ModalModule,
+  UserModule,
+  SplashScreenModule,
+  HomeScreenModule,
+);
+
+registerRootComponent(() => {
+  return <App get={serviceId => container.get(serviceId)} />;
+});
--- src/app.tsx
+++ src/app.tsx
@@ -1,34 +1,30 @@
 import React from 'react';
-import '@/uilib';
+import { interfaces } from 'inversify';
 
 import { AppModule } from '@/di/model';
 import { IRouter } from '@/router';
 
-import { container } from './di';
 import { IModalService } from './modal';
 import { IProcessInfoService } from './process-info';
 
-export class App extends React.Component {
-
-  private router: IRouter;
-  private modalService: IModalService;
-  private processInfoService: IProcessInfoService;
-
-  constructor(props: {}) {
-    super(props);
-    this.router = container.get<IRouter>(AppModule.ROUTER);
-    this.modalService = container.get<IModalService>(AppModule.MODAL);
-    this.processInfoService = container.get<IProcessInfoService>(AppModule.PROCESS_INFO);
-
-    this.processInfoService.startListening();
-  }
-
-  public render(): React.ReactElement {
-    return (
-      <>
-        {this.router.getWindow()}
-        {this.modalService.getWindow()}
-      </>
-    );
-  }
+interface Props {
+  get<T>(id: interfaces.ServiceIdentifier<T>): T;
 }
+
+export const App: React.FC<Props> = ({ get }) => {
+
+  const router = get<IRouter>(AppModule.ROUTER);
+  const modalService = get<IModalService>(AppModule.MODAL);
+  const processInfoService = get<IProcessInfoService>(AppModule.PROCESS_INFO);
+
+  React.useEffect(() => {
+    processInfoService.startListening();
+  }, []);
+
+  return (
+    <>
+      {router.getWindow()}
+      {modalService.getWindow()}
+    </>
+  );
+};

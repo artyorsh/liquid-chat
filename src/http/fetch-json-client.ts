a0cc54bbf4f5dc25306f852af041c19eb0346fdf
Artur Y <artur@artyorsh.me>
2025-09-25 17:21:22 +0200

    refactor(http): cleaner implementation

--- src/http/fetch-json-client.ts
+++ src/http/fetch-json-client.ts
@@ -17,121 +17,102 @@ export class FetchJsonClient implements IHttpClient {
   }
 
   public async get<R>(url: string, config: IReqestConfig): Promise<R> {
+    const requestMethod: RequestInit['method'] = 'GET';
+
     try {
-      const response = await fetch(`${this.baseUrl}${url}`, {
-        ...config,
-        method: 'GET',
-        headers: {
-          ...this.getDefaultHeaders(),
-          ...config.headers,
-        },
-      });
+      const [response, data] = await this.fetchJson<R>(url, requestMethod, config);
 
-      const data = await response.json();
-      this.options.logger?.debug(`GET ${url} ${JSON.stringify({ status: response.status, data }, null, 2)}`);
-
-      const shouldResolve: boolean = this.options?.shouldResolveStatus?.(response.status) ?? true;
-
-      if (!shouldResolve) {
-        throw new Error(response.status.toString());
-      }
+      this.logResponse(url, requestMethod, response, data);
+      this.assertShouldResolve(response.status);
 
       return data;
     } catch (error) {
-      this.options.logger?.error(`GET ${url} ${error}`);
+      this.options.logger?.error(`${requestMethod} ${url} ${error}`);
 
       throw error;
     }
   }
 
   public async post<R>(url: string, config: IReqestConfig): Promise<R> {
+    const requestMethod: RequestInit['method'] = 'POST';
+
     try {
-      const response = await fetch(`${this.baseUrl}${url}`, {
-        ...config,
-        method: 'POST',
-        headers: {
-          ...this.getDefaultHeaders(),
-          ...config.headers,
-        },
-      });
+      const [response, data] = await this.fetchJson<R>(url, requestMethod, config);
 
-      const data = await response.json();
-      this.options.logger?.debug(`POST ${url} ${JSON.stringify({ status: response.status, data }, null, 2)}`);
-
-      const shouldResolve = this.options?.shouldResolveStatus?.(response.status) ?? true;
-
-      if (!shouldResolve) {
-        throw new Error(response.status.toString());
-      }
+      this.logResponse(url, requestMethod, response, data);
+      this.assertShouldResolve(response.status);
 
       return data;
     } catch (error) {
-      this.options.logger?.error(`POST ${url} ${error}`);
+      this.logError(url, requestMethod, error);
 
       throw error;
     }
   }
 
   public async put<R>(url: string, config: IReqestConfig): Promise<R> {
+    const requestMethod: RequestInit['method'] = 'PUT';
+
     try {
-      const response = await fetch(`${this.baseUrl}${url}`, {
-        ...config,
-        method: 'PUT',
-        headers: {
-          ...this.getDefaultHeaders(),
-          ...config.headers,
-        },
-      });
+      const [response, data] = await this.fetchJson<R>(url, requestMethod, config);
 
-      const data = await response.json();
-      this.options.logger?.debug(`PUT ${url} ${JSON.stringify({ status: response.status, data }, null, 2)}`);
-
-      const shouldResolve = this.options?.shouldResolveStatus?.(response.status) ?? true;
-
-      if (!shouldResolve) {
-        throw new Error(response.status.toString());
-      }
+      this.logResponse(url, requestMethod, response, data);
+      this.assertShouldResolve(response.status);
 
       return data;
     } catch (error) {
-      this.options.logger?.error(`PUT ${url} ${error}`);
+      this.logError(url, requestMethod, error);
 
       throw error;
     }
   }
 
   public async delete<R>(url: string, config: IReqestConfig): Promise<R> {
+    const requestMethod: RequestInit['method'] = 'DELETE';
+
     try {
-      const response = await fetch(`${this.baseUrl}${url}`, {
-        ...config,
-        method: 'DELETE',
-        headers: {
-          ...this.getDefaultHeaders(),
-          ...config.headers,
-        },
-      });
+      const [response, data] = await this.fetchJson<R>(url, requestMethod, config);
 
-      const data = await response.json();
-      this.options.logger?.debug(`DELETE ${url} ${JSON.stringify({ status: response.status, data }, null, 2)}`);
-
-      const shouldResolve = this.options?.shouldResolveStatus?.(response.status) ?? true;
-
-      if (!shouldResolve) {
-        throw new Error(response.status.toString());
-      }
+      this.logResponse(url, requestMethod, response, data);
+      this.assertShouldResolve(response.status);
 
       return data;
     } catch (error) {
-      this.options.logger?.error(`DELETE ${url} ${error}`);
+      this.logError(url, requestMethod, error);
 
       throw error;
     }
   }
 
-  private getDefaultHeaders(): Record<string, string> {
-    return {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    };
+  private async fetchJson<R>(path: string, method: RequestInit['method'], config: IReqestConfig): Promise<[Response, R]> {
+    const response: Response = await fetch(`${this.baseUrl}${path}`, {
+      ...config,
+      method,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        ...config.headers,
+      },
+    });
+
+    const data: R = await response.json();
+
+    return [response, data];
+  }
+
+  private assertShouldResolve(status: number): void | never {
+    const shouldResolve: boolean = this.options.shouldResolveStatus?.(status) ?? true;
+
+    if (!shouldResolve) {
+      throw new Error(status.toString());
+    }
+  }
+
+  private logResponse(url: string, requestMethod: RequestInit['method'], response: Response, data: any): void {
+    this.options.logger?.debug(`${requestMethod} ${url} ${JSON.stringify({ status: response.status, data }, null, 2)}`);
+  }
+
+  private logError(url: string, requestMethod: RequestInit['method'], error: Error): void {
+    this.options.logger?.error(`${requestMethod} ${url} ${error}`);
   }
 }

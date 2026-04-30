1ab8ae87bbe0b905ba70d55c0b2689ae2fa169e5
Artur Y <artur@artyorsh.me>
2026-04-30 11:22:35 +0200

    chore(package): lingui@v6
    https://lingui.dev/releases/migration-6
    
    https://lingui.dev/blog/2026/04/22/announcing-lingui-6.0

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,30 @@
 const { pathsToModuleNameMapper } = require('ts-jest');
+const jestExpoPreset = require('jest-expo/jest-preset');
 const { compilerOptions } = require('./tsconfig.json');
 
 const tscPaths = Object.entries(compilerOptions.paths)
   .filter(([key]) => key !== 'react')
   .reduce((acc, [key, value]) => ({...acc, [key]: value }), {})
 
+const babelJestTransform = jestExpoPreset.transform['\\.[jt]sx?$'];
+
+const transformIgnorePatterns = [
+  'react-native',
+  '@react-native',
+  'expo',
+  '@expo',
+  '@react-navigation',
+  '@lingui',
+  '@messageformat',
+];
+
 module.exports = {
   preset: 'jest-expo',
+  // Lingui v6 ships .mjs; jest-expo only transforms .[jt]sx? by default.
+  transform: {
+    ...jestExpoPreset.transform,
+    '^.+\\.mjs$': babelJestTransform,
+  },
   setupFilesAfterEnv: [
     '@testing-library/jest-native/extend-expect',
     'react-native-unistyles/mocks',
@@ -20,6 +38,9 @@ module.exports = {
     '<rootDir>/src/user/user.service.mock.ts',
   ],
   moduleNameMapper: pathsToModuleNameMapper(tscPaths, { prefix: '<rootDir>' }),
+  transformIgnorePatterns: [
+    '/node_modules/(?!(' + transformIgnorePatterns.join('|') + '))',
+  ],
   testMatch: [
     '<rootDir>/src/**/*.spec.(ts|tsx)',
   ],
